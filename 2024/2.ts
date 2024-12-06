32e763a2dba27db8ad580b7c2dad69954a7d9041
Tristan <33831870+TristanKirwan@users.noreply.github.com>
2024-12-07 00:10:55 +0100

    Improved day 2

--- 2024/2.ts
+++ 2024/2.ts
@@ -1,39 +1,49 @@
 function createFrame(names: string[]): string {
-    let longest = "";
+  const longestLength = Math.max(...(names.map(el => el.length)));
+  const border = '*'.repeat(longestLength + 4);
+  const content = names.reduce((acc, curr) => {
+    return acc + `* ${curr}${' '.repeat(longestLength - curr.length)} *\n`
+  }, '')
+  return `${border}\n${content}${border}`
+}
+
+
+// The below was my original version that didn't score very well. I have never really had a practical use for the repeat function, hence all the loops.
+
+// function createFrame(names: string[]): string {
+//     let longest = "";
   
-    names.forEach((name) => {
-      if(name.length > longest.length) longest = name
-    })
+//     names.forEach((name) => {
+//       if(name.length > longest.length) longest = name
+//     })
   
-    const decoratedNames = names.map((name) => {
-      const currentNameLength = name.length
-      let stringOfSameLength = `${name}`;
+//     const decoratedNames = names.map((name) => {
+//       const currentNameLength = name.length
+//       let stringOfSameLength = `${name}`;
       
-      // Add spaces
-      for (let i = currentNameLength; i < longest.length; i++) {
-        stringOfSameLength = `${stringOfSameLength} `
-      };
+//       // Add spaces
+//       for (let i = currentNameLength; i < longest.length; i++) {
+//         stringOfSameLength = `${stringOfSameLength} `
+//       };
   
-      return `* ${stringOfSameLength} *`
-    })
+//       return `* ${stringOfSameLength} *`
+//     })
   
-    let borderDecoration = ``;
-    for (let i = 0; i < longest.length + 4; i++){
-      borderDecoration = `${borderDecoration}*`
-    }
+//     let borderDecoration = ``;
+//     for (let i = 0; i < longest.length + 4; i++){
+//       borderDecoration = `${borderDecoration}*`
+//     }
   
     
-    const allLines = [borderDecoration, ...decoratedNames, borderDecoration];
+//     const allLines = [borderDecoration, ...decoratedNames, borderDecoration];
   
-    let finalString = '';
-    allLines.forEach((line, index) => {
-      finalString += `${line}`
-      if(index !== allLines.length - 1){
-        finalString += `\n`
-      }
-    })
+//     let finalString = '';
+//     allLines.forEach((line, index) => {
+//       finalString += `${line}`
+//       if(index !== allLines.length - 1){
+//         finalString += `\n`
+//       }
+//     })
   
-    return finalString
-  }
-
-  //This initial version didn't score very well.
+//     return finalString
+//   }
